4d17988b5942ea1692c96cad6edcebf5f5e36bfc
Thex <thex-pieddroit@hotmail.com>
2019-10-08 12:47:24 -0400

    Created very basic Cow class

---
+++ Source/Gameplay/Entities/Animals/Cow.h
@@ -0,0 +1,14 @@
+
+#pragma once
+
+#include "Engine/Components/Transform2D.h"
+#include "Engine/Graphics/Shapes/Rectangle.h"
+
+
+class Cow
+{
+private:
+
+	JFF::Transform2D m_transform;
+	JFF::Rectangle m_sprite;
+};
